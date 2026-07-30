242af6a2bfee1496f58bdc3862bb731986cdf2ff
Rohan Singh Negi <rohanwork953@gmail.com>
2026-07-30 18:17:32 +0530

    fixed an d connnection issues

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -4,7 +4,7 @@ import {DB_NAME} from "../constant.js"
 const DbConnection = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(connectionInstance.connection.host)
+        console.log("DB Connection Successful",connectionInstance.connection.host)
     } catch (error) {
         console.log(error)
         process.exit(1)
