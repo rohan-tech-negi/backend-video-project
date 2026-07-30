339aebb73109f7f01c1b63d32ed589dfa05533e6
Rohan Singh Negi <rohanwork953@gmail.com>
2026-07-30 19:32:50 +0530

    applie dthe compare property as well

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -55,12 +55,16 @@ const userModel = new Schema({
 }, {timestamps: true})
 
 // working with the prehook
-userModel.pre("sae", async function (next) {
+userModel.pre("save", async function (next) {
     if(!this.isModified("password")) return next
 
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
 
 
 export const User = mongoose.model("User", userModel)
