feb51ea44a85a453ae218fb9e6cb364336c24425
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-12 00:22:40 +0530

    creatd the refereshtoken as well

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -68,7 +68,7 @@ userSchema.methods.isPasswordCorrect = async function(password) {
 
 
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
             email: this.email,
@@ -82,7 +82,20 @@ userSchema.methods.generateAccessToken = function(){
     )
 }
 
-userSchema.methods.generateRefreshToken = function(){}
+userSchema.methods.generateRefreshToken = function(){
+     return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.userName,
+            fullName: this.fullName
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 
