ed1953800411d7eaa57443fb5abece5303e0fe0f
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-12 00:50:38 +0530

    making the file uploading structure

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({ 
+  cloud_name: 'dq9zasmw3', 
+  api_key: '224779794946545', 
+  api_secret: 'aA6jXRzpsSLEyItXwrjIFCwON-E'
+});
