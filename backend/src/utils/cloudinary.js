da65450b0401d5fa083a60b4296a0ecf24073870
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-12 01:03:42 +0530

    created a cloudinary methods

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -3,7 +3,30 @@ import fs from "fs"
 
 
 cloudinary.config({ 
-  cloud_name: 'dq9zasmw3', 
-  api_key: '224779794946545', 
-  api_secret: 'aA6jXRzpsSLEyItXwrjIFCwON-E'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+
+        // upload the file on cloudianry
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+
+        console.log("File uploaded successfully",response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary}
