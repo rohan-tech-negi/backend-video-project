7595c78a9a7123d9126cd8d6fe8dfe1b9dd547c5
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-15 23:48:24 +0530

    working back on the controller code

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,12 @@ import {asyncHandler}  from "../utils/asyncHandler.js"
 const registerUser = asyncHandler(async(req,res)=>{
      const {userName, fullName, email,password} = req.body
 
+     if(
+        [fullName, userName, email, password].some()
+     ){
+
+     }
+
     
 })
 
