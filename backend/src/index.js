842272fbe08856d6ea0b99b6a434f555a67a349a
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-15 23:25:44 +0530

    fixed some issues from the code

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,22 +2,21 @@
 // dns.setServers(["8.8.8.8", "1.1.1.1"])
 
 import dotenv from "dotenv"
-
 import express from "express"
 import DbConnection from "./db/index.js"
-
+import { app } from "./app.js"
 dotenv.config({
     path: "./.env"
 })
-
-const app = express()
 
 DbConnection()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
         console.log()
+        console.log(`⚙️ Server is running at port : ${process.env.PORT || 8000}`);
     })
 })
 .catch((error)=>{
     
+    console.log("MONGO db connection failed !!! ", error);
 })
