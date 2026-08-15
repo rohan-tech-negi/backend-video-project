1e4ba8e247f379fdb3371874370188c213a73009
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-15 23:40:30 +0530

    done the first task as  get data from the frontend

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import {asyncHandler}  from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler(async(req,res)=>{
+     const {userName, fullName, email,password} = req.body
+
     
 })
 
