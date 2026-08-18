79d812a3dc19abee487ffe7bb458b503548562ca
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-18 21:18:03 +0530

    fixed some few issues

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -62,12 +62,12 @@ userModel.pre("save", async function (next) {
     next()
 })
 
-userSchema.methods.isPasswordCorrect = async function(password) {
+userModel.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password)
 }
 
 
-userSchema.methods.generateAccessToken = function(){
+userModel.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id: this._id,
@@ -82,7 +82,7 @@ userSchema.methods.generateAccessToken = function(){
     )
 }
 
-userSchema.methods.generateRefreshToken = function(){
+userModel.methods.generateRefreshToken = function(){
      return jwt.sign(
         {
             _id: this._id,
