0c57d198e11b11c16d06e3e675f1e176744a4c84
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-18 20:45:03 +0530

    setting uth the images function in the controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -20,7 +20,7 @@ const registerUser = asyncHandler(async(req,res)=>{
        throw new ApiError(409, "User with email or username already exists")
      }
 
-     
+     const avatarLocalPath = req.files?.avatar[0]?.path;
 
     
 })
