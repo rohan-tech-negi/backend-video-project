d06180d03abcc5a80c43ceda7c9baf9b0b87ba85
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-18 20:47:26 +0530

    created our first controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -22,6 +22,11 @@ const registerUser = asyncHandler(async(req,res)=>{
 
      const avatarLocalPath = req.files?.avatar[0]?.path;
 
+     const coverImageLocalPath = req.files?.coverImage[0]?.path
+     if (!avatarLocalPath) {
+         throw new ApiError(400, "Avatar file is required")
+     }
+
     
 })
 
