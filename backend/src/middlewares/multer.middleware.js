62379307edd107c2399cc1dfed3254a0d17f3a20
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-20 12:11:13 +0530

    fixed some few issues from the coe

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,12 +1,15 @@
 import multer from "multer"
-
+import fs from "fs"
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    const dir = './public/temp'
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true })
+    }
+    cb(null, dir)
   },
   filename: function (req, file, cb) {
-    
     cb(null, file.originalname)
   }
 })
