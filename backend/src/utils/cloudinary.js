b54807d31c545b5374e74d97027d7c880db53724
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-20 12:30:35 +0530

    fixing the cloudinary issues

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -22,6 +22,7 @@ const uploadOnCloudinary = async(localFilePath)=>{
         console.log("File uploaded successfully",response.url)
         return response
     } catch (error) {
+        console.error("Cloudinary upload error:", error);
         fs.unlinkSync(localFilePath)
         // remove the locally saved temporary file as the upload operation got failed
         return null;
