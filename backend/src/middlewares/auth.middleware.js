82894a381fe9851414455f1fc348340b3a0e1982
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-25 11:02:05 +0530

    passing the code in the middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,5 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+export const verifyJWT = asyncHandler(async(req,res,next)=>{
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+})
