172bd766dbfc460fdb09a8a85c31b8fc0921f507
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-28 23:36:38 +0530

    started the work

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -81,7 +81,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser  = asyncHandler(async(req,res)=>{
   const {email, username, password} = req.body;
 
-  if(!username || !email){
+  if(!username && !email){
     throw new ApiError(400, "username or email is required")
   }
   const user = User.findOne({
